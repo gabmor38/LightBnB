4635964572db09bcc01238f853ab75f85b97102f
gabmor38 <Gabriela.morenor@gmail.co>
2021-10-13 21:06:06 -0400

    modified addProperty to include SQL

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -94,14 +94,6 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-// const getAllProperties = function(options, limit = 10) {
-//   const limitedProperties = {};
-//   for (let i = 1; i <= limit; i++) {
-//     limitedProperties[i] = properties[i];
-//   }
-//   return Promise.resolve(limitedProperties);
-// }
-// 
 
 const getAllProperties = (options, limit = 10) => {
    // 1
@@ -157,16 +149,23 @@ const getAllProperties = (options, limit = 10) => {
 
 exports.getAllProperties = getAllProperties;
 
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [];
+  const insertQuery =  `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_space, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+
+  queryParams.push(`${property.owner_id}`, `${property.title}`, `${property.description}`, 
+  `${property.thumbnail_photo_url}`, `${property.cover_photo_url}`, `${property.cost_per_night}`, 
+  `${property.street}`, `${property.city}`, `${property.province}`, `${property.post_code}`, 
+  `${property.country}`, `${property.parking_spaces}`, `${property.number_of_bathrooms}`, `${property.number_of_bedrooms}`)
+
+  console.log(insertQuery, queryParams);
+  return pool.query(insertQuery,queryParams). then((res) => res.rows);
+  
 }
 exports.addProperty = addProperty;
